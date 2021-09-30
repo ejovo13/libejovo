1e24dac8a686a7b4da2c2d992de83e4f79989d36
archstation <ejovo13@yahoo.com>
2021-09-30 11:05:24 +0200

    test random routines

--- src/test.c
+++ src/test.c
@@ -12,4 +12,19 @@ int main() {
     for (int i = 0; i < 200; i ++) {
         printf("%d ", get_rand_int_range(5, 10));
     }
+
+    ejovo_seed();
+
+    xor_rng rng;
+    seed_xoshiro256ss(&rng);
+    print_xoshiro256ss_state(&rng);
+    print_xoshiro256ss_state(&XOSHIRO_RNG);
+
+    for(int i = 0; i < 15; i ++) {
+        printf("%d\n", unif(1, 10));
+        print_xoshiro256ss_state(&XOSHIRO_RNG);
+
+    }
+
+
 }
