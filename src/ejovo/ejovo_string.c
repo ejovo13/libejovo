51fa9f4b322606d50dc76e90cedb741b686300d4
archstation <ejovo13@yahoo.com>
2021-10-03 21:53:37 +0200

    Finish exercise 5, refactor 3

--- src/ejovo/ejovo_string.c
+++ src/ejovo/ejovo_string.c
@@ -16,3 +16,63 @@ char * cpy_n_str(char * input, size_t n) {
     strcpy(new_str, input);
     return new_str;
 }
+
+// take an input string and return a copy that has been reversed.
+char * reverse_string(const char * __string) {
+
+    size_t __string_length = strlen(__string);
+    char * duplicate = (char *) malloc (sizeof(char) * (__string_length + 1));
+
+    for (size_t i = 0; i < __string_length; i ++) {
+        duplicate[__string_length - 1 - i] = __string[i];
+    }
+    duplicate[__string_length] = '\0';
+
+    return duplicate;
+
+}
+
+// Tell me how long the string is (without null-terminating character) and then reverse the string
+char * reverse_string_n(const char * __string, size_t __string_length) {
+
+    char * duplicate = (char *) malloc (sizeof(char) * (__string_length + 1));
+
+    for (size_t i = 0; i < __string_length; i ++) {
+        duplicate[__string_length - 1 - i] = __string[i];
+    }
+    duplicate[__string_length] = '\0';
+    return duplicate;
+
+}
+
+// Delete spaces from a string and return a new string who has no spaces
+char * supprime_espaces(const char * __input, size_t * __nb_esp_supp) {
+
+    // return the number of spaces deleted,
+    size_t len = strlen(__input);
+
+    size_t num_spaces = 0;
+    // count number of spaces
+    for (size_t i = 0; i < len; i ++) {
+        if(__input[i] == ' ' || __input[i] == '\n' || __input[i] == '\t') { num_spaces ++; }
+    }
+
+    // initialize a new string whose length is len - num_spaces
+    size_t new_str_capacity = len - num_spaces + 1;
+    char * new_str = (char *) malloc(sizeof(char) * new_str_capacity);
+
+    // Fill new string
+    new_str[new_str_capacity-1] = '\0';
+
+    size_t i_new = 0; // index of new string
+    for (size_t i = 0; i < len; i ++) {
+        if(__input[i] != ' ' && __input[i] != '\n'&& __input[i] != '\t') {
+            new_str[i_new] = __input[i];
+            i_new ++;
+        }
+    }
+
+    *__nb_esp_supp = num_spaces;
+
+    return new_str;
+}
--- src/ex3.c
+++ src/ex3.c
@@ -4,49 +4,17 @@
 #include <stdbool.h>
 #include "ejovo_string.h"
 
-char * supprime_espaces(char * __input, size_t * __nb_esp_supp) {
-
-    // return the number of spaces deleted,
-    size_t len = strlen(__input);
-
-    size_t num_spaces = 0;
-    // count number of spaces
-    for (size_t i = 0; i < len; i ++) {
-        if(__input[i] == ' ' || __input[i] == '\n') { num_spaces ++; }
-    }
-
-    // initialize a new string whose length is len - num_spaces
-    size_t new_str_capacity = len - num_spaces + 1;
-    char * new_str = (char *) malloc(sizeof(char) * new_str_capacity);
-
-    // Fill new string
-    new_str[new_str_capacity-1] = '\0';
-
-    size_t i_new = 0; // index of new string
-    for (size_t i = 0; i < len; i ++) {
-        if(__input[i] != ' ' && __input[i] != '\n') {
-            new_str[i_new] = __input[i];
-            i_new ++;
-        }
-    }
-
-    *__nb_esp_supp = num_spaces;
-
-    return new_str;
-}
-
 int main() {
 
     size_t espaces_supprimes = 0;
     char * new_str = NULL;
 
-    char * old_str = "Su  p bi  tch  !\n asd \n ";
+    char * old_str = "Bon jo u\t r \n\n\tmon\t a  mi   !\n  \n ";
 
+    // supprime_espaces definie en "ejovo_string.c"
     new_str = supprime_espaces(old_str, &espaces_supprimes);
 
-    printf("Converted s:\n %s\n to:\n %s\n n_deleted_spaces: %ld\n", old_str, new_str, espaces_supprimes);
-
-
+    printf("Converted string: \n\n '%s'\n\n to:\n '%s'\n n_deleted_spaces: %ld\n", old_str, new_str, espaces_supprimes);
 
     return 0;
 }
