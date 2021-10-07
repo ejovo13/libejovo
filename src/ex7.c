5618ed5b711919705a4a8d64978d8bc9c0eb7584
archstation <ejovo13@yahoo.com>
2021-10-07 10:56:05 +0200

    add comments

--- src/ex7.c
+++ src/ex7.c
@@ -12,8 +12,8 @@ int main() {
 
     char exp_alg[] = "(a + x - 2)*(e^x - (2 * 2))+(3)";
 
-    printf("Expression: '%s', reduced: '%s'\n", exp_alg, extract_parentheses(exp_alg));
-    printf("est_bien_parenthese(exp_alg) = %d\n", est_bien_parenthesee(exp_alg));
+    printf("Expression: '%s', reduced: '%s'\n", exp_alg, extract_parentheses(exp_alg)); // extract_parenthesis defined in "ejovo_string.h"
+    printf("est_bien_parenthese(exp_alg) = %d\n", est_bien_parenthesee(exp_alg)); // est_bien_parenthesee defined in "ejovo_string.h"
 
 
     return 0;
