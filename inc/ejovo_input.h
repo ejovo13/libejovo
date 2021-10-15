687c139f3bd605f160e958e45d71a1ac2d8b7aad
archstation <ejovo13@yahoo.com>
2021-10-15 17:41:48 +0200

    Add functions to get an integer

---
+++ inc/ejovo_input.h
@@ -0,0 +1,11 @@
+// A Collection of functions used to get input from the user.
+
+// The function prototype is
+//
+// TYPE get_TYPE(void)
+//
+// and will get an appropriate type from the user
+#include <stdbool.h>
+#include <stdio_ext.h>
+
+int get_int(void);
