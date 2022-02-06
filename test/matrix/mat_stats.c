581af6feceab7ab0d9e93f2aca029006af45d986
archstation <ejovo13@yahoo.com>
2022-02-06 02:16:01 +0100

    Verify Matrix tests

--- test/matrix/mat_stats.c
+++ test/matrix/mat_stats.c
@@ -33,12 +33,19 @@ void t_stats() {
 
     Matrix *a = get_A();
 
-    Matrix_print(a);
     Matrix_print(v);
+    Matrix_print(a);
 
-
-
-
+    assert(Matrix_iter_sum(a) == 21);
+    assert(Matrix_iter_mean(a) == 3.5);
+    assert(Matrix_iter_prod(a) == 720);
+    assert(Matrix_iter_mean_squared(a) == 15.166666666666666);
+    assert(Matrix_iter_rms(a) == sqrt(15.166666666666666));
+    assert(Matrix_iter_min(a) == 1);
+    assert(Matrix_iter_max(a) == 6);
+    assert(Matrix_iter_std(a) == 1.707825127659933); // Hell yeah this shit matches exactly with MATLAB's calculation.
+    printf("Var: %lf\n", Matrix_iter_var(a));
+    assert(fabs(Matrix_iter_var(a) - 2.916666666666667) < 0.000000001); // can't get bitwise precision here.
 
     Matrix_free(v);
     Matrix_free(a);
