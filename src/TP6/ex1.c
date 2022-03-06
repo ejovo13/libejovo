6bdb25f1b6fa80740e946a7e634c99669605c45a
archstation <ejovo13@yahoo.com>
2022-03-06 17:20:04 +0100

    Fix header causing Mac build to break

--- src/TP6/ex1.c
+++ src/TP6/ex1.c
@@ -1,5 +1,7 @@
 // D´efinir un type de donn´ee liste dont chaque maillon est compos´e d’un nombre flottant c en
 // double pr´ecision et d’un entier n.
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node_t {
 
