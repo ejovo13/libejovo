2f123d0955d904de3fe78aba3370933e32161d2b
archstation <ejovo13@yahoo.com>
2022-03-08 07:28:05 +0100

    Clean up memory leaks in test

--- test/cpy_str.c
+++ test/cpy_str.c
@@ -7,9 +7,19 @@ int main() {
     char test_str[] = "Hello this is the string that I am going to copy\n";
     size_t str_len = strlen(test_str);
 
-    assert(strcmp(test_str, cpy_str(test_str)) == 0); // strcmp returns 0 if the strings are equal
-    assert(strcmp(test_str, cpy_n_str(test_str, strlen(test_str))) == 0);
-    assert(strcmp(test_str, cpy_n_str(test_str, str_len)) == 0);
+    char *ptr = cpy_str(test_str);
+
+    assert(strcmp(test_str, ptr) == 0); // strcmp returns 0 if the strings are equal
+
+    free(ptr);
+    ptr = cpy_n_str(test_str, strlen(test_str));
+
+    assert(strcmp(test_str, ptr) == 0);
+
+    free(ptr);
+    ptr = cpy_n_str(test_str, str_len);
+    assert(strcmp(test_str, ptr) == 0);
+    free(ptr);
 
     return 0;
 }
