33183dc3d7ee9806b7a84b2be3ff8b4dcbfd8653
Evan Voyles (laptop) <ejovo13@yahoo.com>
2022-03-12 14:42:13 +0100

    Add routines to start playing with stochastic matrices

--- inc/matrix/matrix_stochastic.h
+++ inc/matrix/matrix_stochastic.h
@@ -6,6 +6,17 @@ Matrix *Matrix_runif(size_t __m, size_t __n, double __a, double __b);
 
 Matrix *Matrix_rnorm(size_t __m, size_t __n, double __mean, double __std);
 
+Matrix *as_stochastic(Matrix *__m);
+
 Matrix *Matrix_rexp(size_t __m, size_t __n, double __rate);
 
 Matrix *Matrix_as_stochastic(const Matrix *__m);
+
+// Create a new Stochastic matrix whose elements come from a uniform distribution
+Matrix *Stochastic_runif(size_t __n, double __a, double __b);
+
+Matrix *Stochastic_rnorm(size_t __n, double __mean, double __std);
+
+Matrix *Stochastic_rexp(size_t __n, double __rate);
+
+Vector *Vector_prob_unif(size_t __n);
--- src/matrix/matrix_stochastic.c
+++ src/matrix/matrix_stochastic.c
@@ -36,7 +36,31 @@ Matrix *Matrix_rexp(size_t __m, size_t __n, double __rate) {
     return m;
 }
 
-// This function should TECHNICALLY verify the fact
+// Low level routine that will modify a matrix in place
+Matrix *as_stochastic(Matrix *__m) {
+
+    apply(__m, fabs);
+
+    // No I want to normalize the rows based on their sums!!
+    double sum = 0;
+
+    // So first thing I need to do is iterate the rows
+    for (size_t i = 0; i < __m->nrows; i++) {
+
+        // Get the sum of each row
+        MatIter start = Matrix_row_begin(__m, i);
+        MatIter end   = Matrix_row_end(__m, i);
+
+        sum = MatIter_sum(start, end);
+
+        // Now go and adjust the row
+        MatIter_apply_div_k(start, end, sum);
+    }
+
+    return __m;
+}
+
+// This function should TECHNICALLY verify the fact that the matrix is square.
 Matrix *Matrix_as_stochastic(const Matrix *__m) {
 
     // first thing I should do is apply the absolute value function to the matrix
@@ -60,4 +84,33 @@ Matrix *Matrix_as_stochastic(const Matrix *__m) {
     }
 
     return m_pos;
+}
+
+// Create a new Stochastic matrix whose elements come from a uniform distribution
+Matrix *Stochastic_runif(size_t __n, double __a, double __b) {
+
+    // first thing I need to do is create a new uniformly generated matrix, althought the
+    // actual scale shouldnt really matter...
+    Matrix *stoch = Matrix_runif(__n, __n, __a, __b);
+
+    return as_stochastic(stoch);
+}
+
+Matrix *Stochastic_rnorm(size_t __n, double __mean, double __std) {
+
+    Matrix *stoch = Matrix_rnorm(__n, __n, __mean, __std);
+
+    return as_stochastic(stoch);
+}
+
+Matrix *Stochastic_rexp(size_t __n, double __rate) {
+
+    Matrix *stoch = Matrix_rexp(__n, __n, __rate);
+
+    return as_stochastic(stoch);
+}
+
+// Return a DSICRETE probability vector
+Vector *Vector_prob_unif(size_t __n) {
+    return Matrix_value(__n, 1, 1.0 / __n);
 }
