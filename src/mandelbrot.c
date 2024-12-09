50b24870ab66bac97c75f9d894ad015025d2aa16
Evan Voyles <ejovo13@yahoo.com>
2024-12-09 03:07:49 +0100

    Playing with a new binary

---
+++ src/mandelbrot.c
@@ -0,0 +1,16 @@
+/**
+ * @file mandelbrot.c
+ * @brief  Generate the mandelbro set using a matrix, then output a png file
+ */
+
+#include "matrix_gen.h"
+
+int main() {
+
+    printf("Hello mandelbrot!");
+
+
+
+
+}
+
