3c651f6e06fa68856d4b254c16e8b1474fc40b9a
Evan Voyles <ejovo13@yahoo.com>
2024-12-09 02:40:47 +0100

    Remove unnecessary import

--- src/hello_ejovo.c
+++ src/hello_ejovo.c
@@ -1,13 +1,8 @@
 // Say hello with one of our color functions
 #include "ejovo_color.h"
-#include "ejovo_img.h"
 
 int main() {
     color_red();
     printf("Hello Ejovo :)\n");
     color_reset();
-
-
-
-
 }
